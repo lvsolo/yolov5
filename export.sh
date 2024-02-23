d2b09cc06904052bc85e0fd74216b531530f4f3c
lvsolo <lvsolo@126.com>
2024-02-23 17:50:47 +0800

    add trt tensorrt engine inference and detection in one file support.

--- export.sh
+++ export.sh
@@ -1,1 +1,1 @@
-python3 export.py --weights runs/train/exp15/weights/best.pt --include engine --data data_bdd100k.yaml --device 0 --opset 12 --batch-size 1
+python3 export.py --weights runs/train/exp28/weights/best.pt --include engine --data data_bdd100k.yaml --half --device 0 --opset 12 --batch-size 1
